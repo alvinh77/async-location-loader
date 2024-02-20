93e586bf8b116d1467473fcc76a1068aa076eba7
Alvin H <alvinh1223@gmail.com>
2024-02-20 17:11:52 +1100

    Added codecov and lint

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AsyncLocationLoader",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AsyncLocationLoader",
-            targets: ["AsyncLocationLoader"]),
+            targets: ["AsyncLocationLoader"])
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -19,6 +19,6 @@ let package = Package(
             name: "AsyncLocationLoader"),
         .testTarget(
             name: "AsyncLocationLoaderTests",
-            dependencies: ["AsyncLocationLoader"]),
+            dependencies: ["AsyncLocationLoader"])
     ]
 )
