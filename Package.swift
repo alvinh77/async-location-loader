f4e26f838680fd2cacff6e6b6b7a112394dae3d5
Alvin <12960590+alvinh77@users.noreply.github.com>
2024-10-19 10:13:16 +1100

    Updated gitignore

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,8 +17,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AsyncLocationLoader",
-            swiftSettings: [.enableUpcomingFeature("StrictConcurrency")]
+            name: "AsyncLocationLoader"
         ),
         .testTarget(
             name: "AsyncLocationLoaderTests",
