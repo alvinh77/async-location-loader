cefa6dbc78394a43169f6590a5a49b9515b463d8
Alvin H. <12960590+alvinh77@users.noreply.github.com>
2024-10-19 12:01:23 +1100

    Enabled strict concurrency (#4)
    * Updated gitignore
    
    * Enabled strict concurrency
    
    * Updated github host version

--- Tests/AsyncLocationLoaderTests/Mocks/TestLocationManager.swift
+++ Tests/AsyncLocationLoaderTests/Mocks/TestLocationManager.swift
@@ -8,21 +8,26 @@
 import AsyncLocationLoader
 import CoreLocation
 
-final class TestLocationManager: LocationManaging {
+final class TestLocationManager: LocationManaging, @unchecked Sendable {
+    private let lock = NSLock()
     private(set) var requestWhenInUseAuthorizationCallsCount = 0
     private(set) var requestAlwaysAuthorizationCallsCount = 0
 
-    var authorizationStatus: CLAuthorizationStatus
+    let authorizationStatus: CLAuthorizationStatus
 
     init(authorizationStatus: CLAuthorizationStatus = .notDetermined) {
         self.authorizationStatus = authorizationStatus
     }
 
     func requestWhenInUseAuthorization() {
+        lock.lock()
         requestWhenInUseAuthorizationCallsCount += 1
+        lock.unlock()
     }
 
     func requestAlwaysAuthorization() {
+        lock.lock()
         requestAlwaysAuthorizationCallsCount += 1
+        lock.unlock()
     }
 }
