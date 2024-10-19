214f2d0cf68e85e9d659afa5efaf1a88d769e05a
Alvin <12960590+alvinh77@users.noreply.github.com>
2024-10-19 11:38:49 +1100

    Enabled strict concurrency

--- Tests/AsyncLocationLoaderTests/AsyncLocationLoaderTests.swift
+++ Tests/AsyncLocationLoaderTests/AsyncLocationLoaderTests.swift
@@ -7,55 +7,42 @@
 
 @testable import AsyncLocationLoader
 
-import XCTest
+import CoreLocation
+import Testing
 
-final class AsyncLocationLoaderTests: XCTestCase {
-    func test_authorizationStatus() {
-        XCTAssertEqual(
-            AsyncLocationLoader(
-                locationManager: TestLocationManager(authorizationStatus: .notDetermined)
-            ).authorizationStatus,
-            .notDetermined
+struct AsyncLocationLoaderTests {
+
+    @Test(arguments: [
+        (CLAuthorizationStatus.notDetermined, AuthorizationStatus.notDetermined),
+        (CLAuthorizationStatus.denied, AuthorizationStatus.denied),
+        (CLAuthorizationStatus.restricted, AuthorizationStatus.restricted),
+        (CLAuthorizationStatus.authorizedAlways, AuthorizationStatus.authorizedAlways),
+        (CLAuthorizationStatus.authorizedWhenInUse, AuthorizationStatus.authorizedWhenInUse)
+    ])
+    func authorizationStatus(
+        inputStatus: CLAuthorizationStatus, outputStatus: AuthorizationStatus
+    ) async {
+        let loader = AsyncLocationLoader(
+            locationManager: TestLocationManager(authorizationStatus: inputStatus)
         )
-        XCTAssertEqual(
-            AsyncLocationLoader(
-                locationManager: TestLocationManager(authorizationStatus: .restricted)
-            ).authorizationStatus,
-            .restricted
-        )
-        XCTAssertEqual(
-            AsyncLocationLoader(
-                locationManager: TestLocationManager(authorizationStatus: .denied)
-            ).authorizationStatus,
-            .denied
-        )
-        XCTAssertEqual(
-            AsyncLocationLoader(
-                locationManager: TestLocationManager(authorizationStatus: .authorizedAlways)
-            ).authorizationStatus,
-            .authorizedAlways
-        )
-        XCTAssertEqual(
-            AsyncLocationLoader(
-                locationManager: TestLocationManager(authorizationStatus: .authorizedWhenInUse)
-            ).authorizationStatus,
-            .authorizedWhenInUse
-        )
+        await #expect(loader.authorizationStatus == outputStatus)
     }
 
-    func test_requestAuthorizationForAlways_callsLocationManager() async {
+    @Test
+    func requestAuthorizationForAlways_callsLocationManager() async {
         let locationManager = TestLocationManager(authorizationStatus: .authorizedAlways)
         let locationLoader = AsyncLocationLoader(locationManager: locationManager)
         let status = await locationLoader.requestAuthorization(for: .always)
-        XCTAssertEqual(status, .authorizedAlways)
-        XCTAssertEqual(locationManager.requestAlwaysAuthorizationCallsCount, 1)
+        #expect(status == .authorizedAlways)
+        #expect(locationManager.requestAlwaysAuthorizationCallsCount == 1)
     }
 
-    func test_requestAuthorizationForWhenInUse_callsLocationManager() async {
+    @Test
+    func requestAuthorizationForWhenInUse_callsLocationManager() async {
         let locationManager = TestLocationManager(authorizationStatus: .authorizedWhenInUse)
         let locationLoader = AsyncLocationLoader(locationManager: locationManager)
         let status = await locationLoader.requestAuthorization(for: .whenInUse)
-        XCTAssertEqual(status, .authorizedWhenInUse)
-        XCTAssertEqual(locationManager.requestWhenInUseAuthorizationCallsCount, 1)
+        #expect(status == .authorizedWhenInUse)
+        #expect(locationManager.requestWhenInUseAuthorizationCallsCount == 1)
     }
 }
