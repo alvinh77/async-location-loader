ed4a89b819117ab6e93f2a5a35dc627c6b29d314
Alvin <12960590+alvinh77@users.noreply.github.com>
2024-10-20 11:07:32 +1100

    Added template

---
+++ ExampleApp/ExampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Alvin He on 20/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
