92255d8e628e36ce006ed441ee1e2c96a170e035
Alvin H. <12960590+alvinh77@users.noreply.github.com>
2024-10-20 19:59:03 +1100

    Added example project (#6)
    * Added template
    
    * Integrated logics

---
+++ ExampleApp/ExampleApp/ExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  ExampleApp.swift
+//  ExampleApp
+//
+//  Created by Alvin He on 20/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                ContentView()
+            }
+        }
+    }
+}
--- Tests/AsyncLocationLoaderTests/Mocks/TestLocationManager.swift
+++ Tests/AsyncLocationLoaderTests/Mocks/TestLocationManager.swift
@@ -9,25 +9,33 @@ import AsyncLocationLoader
 import CoreLocation
 
 final class TestLocationManager: LocationManaging, @unchecked Sendable {
+    var authorizationStatus: CLAuthorizationStatus {
+        get { lock.withLock { _authorizationStatus } }
+        set { lock.withLock { _authorizationStatus = newValue } }
+    }
+    var delegate: (any CLLocationManagerDelegate)? {
+        get { lock.withLock { _delegate } }
+        set { lock.withLock { _delegate = newValue } }
+    }
+    private var _authorizationStatus: CLAuthorizationStatus
+    private var _delegate: (any CLLocationManagerDelegate)?
     private let lock = NSLock()
     private(set) var requestWhenInUseAuthorizationCallsCount = 0
     private(set) var requestAlwaysAuthorizationCallsCount = 0
 
-    let authorizationStatus: CLAuthorizationStatus
-
     init(authorizationStatus: CLAuthorizationStatus = .notDetermined) {
-        self.authorizationStatus = authorizationStatus
+        self._authorizationStatus = authorizationStatus
     }
 
     func requestWhenInUseAuthorization() {
-        lock.lock()
-        requestWhenInUseAuthorizationCallsCount += 1
-        lock.unlock()
+        lock.withLock {
+            requestWhenInUseAuthorizationCallsCount += 1
+        }
     }
 
     func requestAlwaysAuthorization() {
-        lock.lock()
-        requestAlwaysAuthorizationCallsCount += 1
-        lock.unlock()
+        lock.withLock {
+            requestAlwaysAuthorizationCallsCount += 1
+        }
     }
 }
